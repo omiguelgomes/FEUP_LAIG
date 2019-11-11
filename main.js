c8b5e48f366f2b38a8ba2216886b40706bbe4e55
Edu09campos <eddycampos09@hotmail.com>
2019-11-11 14:46:35 +0000

    finished 2d/3d surfaces

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function getUrlVars() {
 //Include additional files here
 serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 
     'MyInterface.js', 'MyRectangle.js', 'MySphere.js', 'MyCylinder.js', 
-    'MyTorus.js', 'MyTriangle.js', 'MyComponent.js',  
+    'MyTorus.js', 'MyTriangle.js', 'MyComponent.js', 'MyPlane.js', 'MyPatch.js', 'MyCylinder2.js', 
 
 main=function()
 {
